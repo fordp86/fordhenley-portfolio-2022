10e8799742f575267253867c87110026a2e6b1ba
Ford Henley <fordp86@gmail.com>
2022-06-17 11:59:41 -0500

    updated nextjs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17e282c29e5c3646"; }
+/******/ 		__webpack_require__.h = function() { return "6bfaeb0cc413cbb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
